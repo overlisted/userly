336c1456432595cb192a007b04f26bcdec7a5c37
Ilya Maximov <mail@overlisted.net>
2021-02-08 23:07:06 +0100

    finish email verification

--- services/emailVerification.js
+++ services/emailVerification.js
@@ -1,12 +1,27 @@
 const { newUser } = require("./user");
 const { v4: generateUuid } = require("uuid");
+const { sendEmail } = require("./emailing");
+const pug = require("pug");
 
 let unverifiedUsers = new Map(); //: Map<string, { email: string, username: string, password: string }>
+
+const makeVerificationHTML = pug.compileFile("verificationEmail.pug");
+const sendVerification = async ticket => {
+  const user = unverifiedUsers.get(ticket);
+
+  await sendEmail(
+    process.env.EMAIL_VERIFICATION_FROM,
+    user.email,
+    "Email Verification",
+    makeVerificationHTML({username: user.username, ticket})
+  )
+};
 
 const beginVerification = async (email, username, password) => {
   const ticket = generateUuid();
 
   unverifiedUsers.set(ticket, {email, username, password});
+  await sendVerification(ticket);
 };
 
 const verify = async ticket => {
